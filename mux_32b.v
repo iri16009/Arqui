3ce3409aaa93226e577b4fe9a4e3698c1f403935
iri16009 <iri16009@uvg.edu.gt>
2019-10-30 14:34:25 -0600

    Extender
    Agregué el extender al archivo mux_32b.v

--- mux_32b.v
+++ mux_32b.v
@@ -18,3 +18,9 @@ module mux5(input [4:0] a, b, input select, output [4:0] y);
 assign y = (select) ? a: b;
 
 endmodule
+
+module extender (input [15:0] a, output [31:0] y);
+
+assign y = {16'b0, a};
+
+endmodule // extender
